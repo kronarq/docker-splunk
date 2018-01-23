d890cf01fa7c87b4b1ec55c5389a62b1b8c6ace0
Joshua Hansen <kronarq@gmail.com>
2018-01-23 14:56:24 -0700

    Cleanup

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
+#!/usr/bin/env bash
+
 #https://docs.docker.com/engine/reference/commandline/build/
 if [ -z $CURRENT ]; then
 	CURRENT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 fi
 
-docker build --no-cache=true -t joshuahansen/splunk:7.0.0 .
-docker tag splunk/splunk:7.0.0 joshuahansen/splunk:latest 
-docker tag splunk/splunk:7.0.0 registry.splunk.com/joshuahansen/splunk:latest 
-docker tag splunk/splunk:7.0.0 registry.splunk.com/joshuahansen/splunk:7.0.0 
+docker build --no-cache=true -t kronarq/splunk:7.0.0 .
+docker tag kronarq/splunk:7.0.0 kronarq/splunk:latest
--- publishImage.sh
+++ publishImage.sh
@@ -1,7 +1,6 @@
+#!/usr/bin/env bash
 
-docker push splunk/splunk:7.0.0
-docker push splunk/splunk:latest
-docker push registry.splunk.com/splunk/splunk:7.0.0
-docker push registry.splunk.com/splunk/splunk:latest
+docker push kronarq/splunk:7.0.0
+docker push kronarq/splunk:latest
 
 
